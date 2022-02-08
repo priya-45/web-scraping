3574d3347e72eeaba59415fb574678f0b72d7a5a
priya-45 <98081072+priya-45@users.noreply.github.com>
2022-02-08 12:47:22 +0530

    Add files via upload

---
+++ task11.py
@@ -0,0 +1,25 @@
+import json
+with open("task5.json","r")as f:
+    data=json.load(f)
+def analyse_movie_director():
+    director_dict={}
+    for i in data:
+        if "Genre" in i:
+            a=i["Genre"]
+            i=0
+            b=[]
+            while i<len(a):
+                c=a[i].replace(' ','')
+                b.append(c)
+                i=i+1
+            print(b)
+            for j in b:
+                if j not in director_dict:
+                    director_dict[j]=1
+                else:
+                    director_dict[j]+=1
+        else:
+            pass     
+    with open("task11.json","w+") as language_data:
+        json.dump(director_dict,language_data,indent=4)
+analyse_movie_director()
